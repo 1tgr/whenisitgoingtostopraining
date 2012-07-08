2cae4fb3e39fc875e7693a18250b6ac4c51fa236
Tim Robinson <tim.g.robinson@gmail.com>
2012-07-08 12:07:10 +0100

    Geolocation

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
-/* Author:
-
-*/
-
-
-
-
-
-
-
+$(function() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(l) {
+            $("#status").text(l.coords.latitude + ", " + l.coords.longitude);
+        });
+    }
+});
