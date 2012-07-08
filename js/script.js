fa475332f60465baad4808d14e05179777c018b1
Tim Robinson <tim.g.robinson@gmail.com>
2012-07-08 12:47:51 +0100

    Templating

--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
+// Simple JavaScript Templating
+// John Resig - http://ejohn.org/ - MIT Licensed
+(function(){
+  var cache = {};
+  
+  this.tmpl = function tmpl(str, data){
+    // Figure out if we're getting a template, or if we need to
+    // load the template - and be sure to cache the result.
+    var fn = !/\W/.test(str) ?
+      cache[str] = cache[str] ||
+        tmpl(document.getElementById(str).innerHTML) :
+      
+      // Generate a reusable function that will serve as a template
+      // generator (and which will be cached).
+      new Function("obj",
+        "var p=[],print=function(){p.push.apply(p,arguments);};" +
+        
+        // Introduce the data as local variables using with(){}
+        "with(obj){p.push('" +
+        
+        // Convert the template into pure JavaScript
+        str
+          .replace(/[\r\t\n]/g, " ")
+          .split("<%").join("\t")
+          .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+          .replace(/\t=(.*?)%>/g, "',$1,'")
+          .split("\t").join("');")
+          .split("%>").join("p.push('")
+          .split("\r").join("\\'")
+      + "');}return p.join('');");
+    
+    // Provide some basic currying to the user
+    return data ? fn( data ) : fn;
+  };
+})();
+
+
 function when(days) {
     var i, date;
 
@@ -5,35 +42,49 @@ function when(days) {
         if (days[i].pop < 50) {
             date = new Date(0);
             date.setUTCSeconds(parseInt(days[i].date.epoch, 10));
-            return $.timeago(date);
+            return date;
         }
     }
 
-    return "Never";
+    return undefined;
+}
+
+function parse(json) {
+    return {
+        place: json.location.city,
+        date: when(json.forecast.simpleforecast.forecastday)
+    };
 }
 
 $(function() {
     $.timeago.settings.allowFuture = true;
 
-    var status = $("#status");
-    status.ajaxError(function(error) {
-        $(this).text(error);
-    });
+    $.fn.handleError = function(error) {
+        if (error.description === undefined) {
+            error = { description: error };
+        }
+
+        $(this).html(tmpl("error_template", error));
+    };
+
+    var mainElem = $("#main");
+    mainElem.ajaxError($.fn.handleError);
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(l) {
-            status.text("Forecasting weather...");
+            mainElem.html(tmpl("got_location_template"));
 
             var url = "http://api.wunderground.com/api/4c0975b4bf6b69d3/geolookup/forecast/q/" + l.coords.latitude + "," + l.coords.longitude + ".json?callback=?"
             $.getJSON(url, function(json) {
                 if (json.error === undefined) {
-                    status.text(when(json.forecast.simpleforecast.forecastday));
+                    var model = parse(json);
+                    mainElem.html(tmpl(model.date === undefined ? "never_template" : "got_forecast_template", model));
                 } else {
-                    status.text(json.error.description);
+                    mainElem.handleError(json.error);
                 }
             });
         }, function(error) {
-            status.text(error);
+            mainElem.handleError(json.error);
         });
     }
 });
